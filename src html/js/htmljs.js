5e7837f209d9a2476b1d0e1901cfe3dbfa44d9a3
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-08-30 18:01:38 -0300

    atualiza

--- src html/js/htmljs.js
+++ src html/js/htmljs.js
@@ -1,14 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const openPopupButton = document.getElementById("openPopup");
-    const closePopupButton = document.getElementById("closePopup");
-    const popup = document.getElementById("popup");
-  
-    openPopupButton.addEventListener("click", () => {
-      popup.style.display = "flex";
-    });
-  
-    closePopupButton.addEventListener("click", () => {
-      popup.style.display = "none";
-    });
-  });
-  
+const modal = document.querySelector('.modal-container')
+
+function openModal() {
+  modal.classList.add('active')
+}
+
+function closeModal() {
+  modal.classList.remove('active')
+}
