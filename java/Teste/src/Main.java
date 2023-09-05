d3b6108e18f5668ab133ebd0c71464ce06c660e6
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-09-05 19:38:47 -0300

    teste

--- java/Teste/src/Main.java
+++ java/Teste/src/Main.java
@@ -1,17 +1,33 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
-
-public class Main {
+public class Main{
     public static void main(String[] args) {
         try {
-            File input = new File("caminho/do/seu/arquivo.html"); // Substitua pelo caminho correto do seu arquivo HTML
-            Document doc = Jsoup.parse(input, "UTF-8"); // Carrega o arquivo HTML usando o Jsoup
+            // Carregar o arquivo HTML
+            Document document = Jsoup.parse(new File("../../src html/teste copy.html"), "UTF-8");
+
+            // Agora você tem o documento HTML pronto para edição
+            // Você pode fazer várias operações, como buscar elementos e editar seu conteúdo
+
+            // Por exemplo, você pode alterar o conteúdo de um elemento:
+            document.select("div").first().text("Novo Título");
+
+            // Ou adicionar um novo elemento:
+            document.body().append("<p>Este é um novo parágrafo.</p>");
+
+            // Salvar as alterações de volta no arquivo HTML
+            FileWriter fileWriter = new FileWriter("exemplo.html");
+            fileWriter.write(document.outerHtml());
+            fileWriter.close();
+
+            System.out.println("Arquivo HTML editado com sucesso.");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
